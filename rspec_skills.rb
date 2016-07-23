86abf966b165b627cabe4da35c79e8721e14b9d6
Greg Smith <crashingwaves@fea.st>
2016-07-23 08:07:29 -0600

    Add RSpec skills

---
+++ rspec_skills.rb
@@ -0,0 +1,4 @@
+# Given... when... then.
+# Think in user stories
+# You only have to test the parts you want to work
+# Write tests for the happy path, then the unhappy paths, the edge cases, then bugs that pop up
