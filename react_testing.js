3afe50f17df821857771ed1132efb9da29cf0bc4
Gregory Smith <crashingwaves@fea.st>
2016-08-27 16:34:47 -0600

    Add react_testing

---
+++ react_testing.js
@@ -0,0 +1,4 @@
+//React components are basic JS
+//Shallow rendering is the compiling of React components into JavaScript from JSX.  Shallow renders are aware of the React component's name, props, and state, but not the element that the component renders to.
+//Static rendering creates the final HTML which can then be probed by helper functions.  This rendering is **not** aware of the React component's workings, but we do get a full virtual DOM to probe.
+//Full DOM rendering allows the component to interact with an actual DOM in the tests. Because it's the actual DOM structure with React in it, it will be aware of both React and the elements it renders.
